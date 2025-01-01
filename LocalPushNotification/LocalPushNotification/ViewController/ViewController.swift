2b1acbda84d15f1625ec055e4b1248aa47397ae2
Jaymin <jayminsolanki12Qgmail.com>
2025-01-01 10:58:40 +0530

    commit Update code improve

--- LocalPushNotification/LocalPushNotification/ViewController/ViewController.swift
+++ LocalPushNotification/LocalPushNotification/ViewController/ViewController.swift
@@ -1,54 +1,58 @@
 //
 //  ViewController.swift
-//  LocalPushNotification
+//  PushNotification
 //
-//  Created by Jaymin on 25/11/24.
+//  Created by Jaymin on 31/12/24.
 //
 
 import UIKit
 import UserNotifications
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Iboutlet
+    
+    //MARK: - Local variable
+    
+    //MARK: -  view life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         checkForPermission()
     }
-
     
+    //MARK: - checkForPermissionFor Notification
     func checkForPermission(){
-        
         let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.getNotificationSettings { settings in
-            switch settings.authorizationStatus{
-            case .authorized :
+        notificationCenter.getNotificationSettings { setting in
+            switch setting.authorizationStatus{
+            case .authorized:
                 self.dispatchNotification()
             case .denied:
                 return
             case .notDetermined:
                 notificationCenter.requestAuthorization(options: [.alert, .sound]) { didAllow, error in
-                    
-                    if didAllow{
+                    if didAllow {
                         self.dispatchNotification()
                     }
                 }
             default:
                 return
+                
             }
         }
     }
     
+    //MARK: - dispatchNotificationForNotification
     func dispatchNotification(){
-        let identifier = "my-morning-notification"
+        let identifier = "my-mornig-notification"
         let title = "Time to work out!"
-        let body = "Don't be a lazy little butt!"
-        let hour =  16
-        let minute = 25
+        let body = "Don't be a lazy little but!"
+        let hour  = 18
+        let minute = 13
         let isDaily = true
         
         let notificationCenter = UNUserNotificationCenter.current()
+        
         let content = UNMutableNotificationContent()
         content.title = title
         content.body = body
@@ -58,13 +62,15 @@ class ViewController: UIViewController {
         var dateComponents = DateComponents(calendar: calender, timeZone: TimeZone.current)
         dateComponents.hour = hour
         dateComponents.minute = minute
-
+        
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: isDaily)
         let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
         
         notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
         notificationCenter.add(request)
+        
     }
-
+    
 }
+
 
